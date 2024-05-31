7e4d0ee50bbd14e3069e75df40e7483b36a5d210
josechirinos11 <josechirinos11@gmail.com>
2024-05-31 22:27:39 +0200

    carga actualizacion de curriculum para junio

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -55,8 +55,8 @@ const Navbar = ({
     const handleDownload = () => {
         // Crear un enlace de descarga invisible
         const link = document.createElement('a');
-        link.href = '/CVJoseChirinos2024.pdf'; // Ruta al archivo PDF
-        link.download = 'CVJoseChirinos2024.pdf'; // Nombre del archivo al descargar
+        link.href = '/CVJoseChirinos.pdf'; // Ruta al archivo PDF
+        link.download = 'CVJoseChirinos.pdf'; // Nombre del archivo al descargar
         document.body.appendChild(link);
         link.click();
         //document.body.removeChild(link); // Limpieza después de la descarga
